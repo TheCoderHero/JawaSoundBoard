78da3db4881cab74d9824470368bd76e7b665f47
John Tanner <thecoderhero@gmail.com>
2020-07-07 12:01:35 -0700

    Initial Commit
    Contains all working Files. Need to optimize for Web Browser.

---
+++ jawa.js
@@ -0,0 +1,321 @@
+
+//Very Long Clips 7 seconds
+var jawa1 = new Audio();
+jawa1.src = 'wav/jawa1.wav';
+
+var jawa2 = new Audio();
+jawa2.src = 'wav/jawa2.wav';
+
+var jawa3 = new Audio();
+jawa3.src = 'wav/jawa3.wav';
+
+var jawa4 = new Audio();
+jawa4.src = 'wav/jawa4.wav';
+
+var jawa5 = new Audio();
+jawa5.src = 'wav/jawa5.wav';
+
+//Long Clips 2-3 seconds
+//Row 1
+var jawa6 = new Audio();
+jawa6.src = 'wav/jawaA1.wav';
+
+var jawa7 = new Audio();
+jawa7.src = 'wav/jawaA2.wav';
+
+var jawa8 = new Audio();
+jawa8.src = 'wav/jawaA3.wav';
+
+var jawa9 = new Audio();
+jawa9.src = 'wav/jawaA4.wav';
+
+var jawa10 = new Audio();
+jawa10.src = 'wav/jawaA5.wav';
+
+//Row 2
+var jawa11 = new Audio();
+jawa11.src = 'wav/jawaB1.wav';
+
+var jawa12 = new Audio();
+jawa12.src = 'wav/jawaB2.wav';
+
+var jawa13 = new Audio();
+jawa13.src = 'wav/jawaB3.wav';
+
+var jawa14 = new Audio();
+jawa14.src = 'wav/jawaB4.wav';
+
+var jawa15 = new Audio();
+jawa15.src = 'wav/jawaB5.wav';
+
+//Row 3
+var jawa16 = new Audio();
+jawa16.src = 'wav/jawaC1.wav';
+
+var jawa17 = new Audio();
+jawa17.src = 'wav/jawaC2.wav';
+
+var jawa18 = new Audio();
+jawa18.src = 'wav/jawaC3.wav';
+
+var jawa19 = new Audio();
+jawa19.src = 'wav/jawaC4.wav';
+
+var jawa20 = new Audio();
+jawa20.src = 'wav/jawaC5.wav';
+
+//Row 4
+var jawa21 = new Audio();
+jawa21.src = 'wav/jawaD1.wav';
+
+var jawa22 = new Audio();
+jawa22.src = 'wav/jawaD2.wav';
+
+var jawa23 = new Audio();
+jawa23.src = 'wav/jawaD3.wav';
+
+var jawa24 = new Audio();
+jawa24.src = 'wav/jawaD4.wav';
+
+var jawa25 = new Audio();
+jawa25.src = 'wav/jawaD5.wav';
+
+//Row 5
+var jawa26 = new Audio();
+jawa26.src = 'wav/jawaE1.wav';
+
+var jawa27 = new Audio();
+jawa27.src = 'wav/jawaE2.wav';
+
+var jawa28 = new Audio();
+jawa28.src = 'wav/jawaE3.wav';
+
+var jawa29 = new Audio();
+jawa29.src = 'wav/jawaE4.wav';
+
+var jawa30 = new Audio();
+jawa30.src = 'wav/jawaE5.wav';
+
+//Row 6
+var jawa31 = new Audio();
+jawa31.src = 'wav/jawaF1.wav';
+
+var jawa32 = new Audio();
+jawa32.src = 'wav/jawaF2.wav';
+
+var jawa33 = new Audio();
+jawa33.src = 'wav/jawaF3.wav';
+
+//Short Clips 1 second
+//Row 1
+var jawa34 = new Audio();
+jawa34.src = 'wav/jawa_34.wav';
+
+var jawa35 = new Audio();
+jawa35.src = 'wav/jawa_35.wav';
+
+var jawa36 = new Audio();
+jawa36.src = 'wav/jawa_36.wav';
+
+var jawa37 = new Audio();
+jawa37.src = 'wav/jawa_37.wav';
+
+var jawa38 = new Audio();
+jawa38.src = 'wav/jawa_38.wav';
+
+//Row 2
+var jawa39 = new Audio();
+jawa39.src = 'wav/jawa_39.wav';
+
+var jawa40 = new Audio();
+jawa40.src = 'wav/jawa_40.wav';
+
+var jawa41 = new Audio();
+jawa41.src = 'wav/jawa_41.wav';
+
+var jawa42 = new Audio();
+jawa42.src = 'wav/jawa_42.wav';
+
+var jawa43 = new Audio();
+jawa43.src = 'wav/jawa_43.wav';
+
+//General Sound Effects
+//Row 1
+var jawa44 = new Audio();
+jawa44.src = 'wav/jawa_44.wav';
+
+var jawa45 = new Audio();
+jawa45.src = 'wav/jawa_45.wav';
+
+var jawa46 = new Audio();
+jawa46.src = 'wav/jawa_46.wav';
+
+var jawa47 = new Audio();
+jawa47.src = 'wav/jawa_47.wav';
+
+var jawa48 = new Audio();
+jawa48.src = 'wav/jawa_48.wav';
+
+//Row 2
+var jawa49 = new Audio();
+jawa49.src = 'wav/jawa_49.wav';
+
+var jawa50 = new Audio();
+jawa50.src = 'wav/jawa_50.wav';
+
+function playSound(num) {
+    switch(num) {
+        case 1:
+            jawa1.play()
+            break;
+        case 2:
+            jawa2.play()
+            break;
+        case 3:
+            jawa3.play()
+            break;
+        case 4:
+            jawa4.play()
+            break;
+        case 5:
+            jawa5.play()
+            break;
+        case 6:
+            jawa6.play()
+            break;
+        case 7:
+            jawa7.play()
+            break;
+        case 8:
+            jawa8.play()
+            break;
+        case 9:
+            jawa9.play()
+            break;
+        case 10:
+            jawa10.play()
+            break;
+        case 11:
+            jawa11.play()
+            break;
+        case 12:
+            jawa12.play()
+            break;
+        case 13:
+            jawa13.play()
+            break;
+        case 14:
+            jawa14.play()
+            break;
+        case 15:
+            jawa15.play()
+            break;
+        case 16:
+            jawa16.play()
+            break;
+        case 17:
+            jawa17.play()
+            break;
+        case 18:
+            jawa18.play()
+            break;
+        case 19:
+            jawa19.play()
+            break;
+        case 20:
+            jawa20.play()
+            break;
+        case 21:
+            jawa21.play()
+            break;
+        case 22:
+            jawa22.play()
+            break;
+        case 23:
+            jawa23.play()
+            break;
+        case 24:
+            jawa24.play()
+            break;
+        case 25:
+            jawa25.play()
+            break;
+        case 26:
+            jawa26.play()
+            break;
+        case 27:
+            jawa27.play()
+            break;
+        case 28:
+            jawa28.play()
+            break;
+        case 29:
+            jawa29.play()
+            break;
+        case 30:
+            jawa30.play()
+            break;
+        case 31:
+            jawa31.play()
+            break;
+        case 32:
+            jawa32.play()
+            break;
+        case 33:
+            jawa33.play()
+            break;
+        case 34:
+            jawa34.play()
+            break;
+        case 35:
+            jawa35.play()
+            break;
+        case 36:
+            jawa36.play()
+            break;
+        case 37:
+            jawa37.play()
+            break;
+        case 38:
+            jawa38.play()
+            break;
+        case 39:
+            jawa39.play()
+            break;
+        case 40:
+            jawa40.play()
+            break;
+        case 41:
+            jawa41.play()
+            break;
+        case 42:
+            jawa42.play()
+            break;
+        case 43:
+            jawa43.play()
+            break;
+        case 44:
+            jawa44.play()
+            break;
+        case 45:
+            jawa45.play()
+            break;
+        case 46:
+            jawa46.play()
+            break;
+        case 47:
+            jawa47.play()
+            break;
+        case 48:
+            jawa48.play()
+            break;
+        case 49:
+            jawa49.play()
+            break;
+        case 50:
+            jawa50.play()
+            break;
+        default:
+            break;
+    }
+}
